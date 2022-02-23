4b78c1b77ef34cf02b49b8fe58b74d9b2d252e60
Mustafa Gözcü <gozcummustafa@gmail.com>
2022-02-23 09:22:48 +0300

    updated

--- CheckerboardProject/Form1.Designer.cs
+++ CheckerboardProject/Form1.Designer.cs
@@ -34,9 +34,15 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
             this.ClientSize = new System.Drawing.Size(675, 619);
+            this.ControlBox = false;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Checkerboard";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
